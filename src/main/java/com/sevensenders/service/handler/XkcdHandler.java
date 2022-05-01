af02e53225baf4cfe35cd388e0e8ff8746870605
Jeffrytomabraham <jeffryabraham1993@gmail.com>
2022-05-01 18:41:02 +0530

    Xkcd comic feed implemented

---
+++ src/main/java/com/sevensenders/service/handler/XkcdHandler.java
@@ -0,0 +1,83 @@
+package com.sevensenders.service.handler;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.sevensenders.pojo.RssFeedResponse;
+
+@Component
+public class XkcdHandler {
+
+	@Value("${xkcd.comics.urltemplate}")
+	private String xkcdUrlTemplate;
+	
+	public List<RssFeedResponse> getXkcdDetails(){
+		List<RssFeedResponse> feedResponses = new ArrayList<>();
+		try {
+			IntStream.rangeClosed(0, 9).forEach(in -> {
+				String urlString;
+				if(in==0) 
+					urlString = String.format(xkcdUrlTemplate, ""); 
+				else
+					urlString = String.format(xkcdUrlTemplate, in+"/"); 
+				JsonObject jsonData = getJsonObjectFromXkcd(urlString);
+				populateComicDetails(jsonData,feedResponses);
+			});
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return feedResponses;
+	}
+	
+	private void populateComicDetails(JsonObject jsonData,List<RssFeedResponse> feedResponses) {
+		RssFeedResponse rssFeedResponse = new RssFeedResponse();
+		rssFeedResponse.setTitle(jsonData.get("title")!=null ? jsonData.get("title").getAsString() : null);
+		rssFeedResponse.setDescription(jsonData.get("alt")!=null ? jsonData.get("alt").getAsString() : null);
+		rssFeedResponse.setWebUrl(jsonData.get("link")!=null ? jsonData.get("link").getAsString() : null);
+		rssFeedResponse.setImageUrl(jsonData.get("img")!=null ? jsonData.get("img").getAsString() : null);
+		rssFeedResponse.setPublishingDate(generateDateFromJson(jsonData));
+		feedResponses.add(rssFeedResponse);
+	}
+	
+	private LocalDateTime generateDateFromJson(JsonObject jsonData) {
+		int day = jsonData.get("day")!=null ? jsonData.get("day").getAsInt() : 0;
+		int month = jsonData.get("month")!=null ? jsonData.get("month").getAsInt() : 0;
+		int year = jsonData.get("year")!=null ? jsonData.get("year").getAsInt() : 0;
+		LocalDateTime pubDate = null;
+		if(day>0 && month>0 && year>0) {
+			pubDate = LocalDateTime.of(year, month, day, 0, 0, 0);
+		}
+		return pubDate;
+	}
+	
+	private JsonObject getJsonObjectFromXkcd(String urlString) {
+		InputStreamReader reader = null;
+		JsonObject jsonData = null;
+	    try {
+	      URL url = new URL(urlString);
+	      reader = new InputStreamReader(url.openStream(), Charset.forName("UTF-8"));
+	      jsonData = new Gson().fromJson(reader, JsonObject.class);
+	    } catch(Exception ex){
+	    	
+	    }finally {
+	      try {
+	    	if(reader!=null)
+	    		reader.close();
+		  } catch (IOException e) {
+			e.printStackTrace();
+		  }
+	    }
+	    return jsonData;
+	}
+}
--- src/main/java/com/sevensenders/service/impl/RssFeedServiceImpl.java
+++ src/main/java/com/sevensenders/service/impl/RssFeedServiceImpl.java
@@ -1,7 +1,9 @@
 package com.sevensenders.service.impl;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,21 +11,33 @@ import org.springframework.stereotype.Service;
 import com.sevensenders.pojo.RssFeedResponse;
 import com.sevensenders.service.RssFeedService;
 import com.sevensenders.service.handler.FeedBurnerHandler;
+import com.sevensenders.service.handler.XkcdHandler;
 
 @Service
 public class RssFeedServiceImpl implements RssFeedService {
 
 	@Autowired
 	private FeedBurnerHandler feedBurnerHandler;
+	
+	@Autowired
+	private XkcdHandler XkcdHandler;
 
 	public List<RssFeedResponse> getComicFeeds() {
 		List<RssFeedResponse> feedResponses = new ArrayList<>();
 		generateFeedBurnerDetails(feedResponses);
-		return feedResponses;
+		generateXkcdDetails(feedResponses);
+		List<RssFeedResponse> sortedResponse = feedResponses.stream()
+				.sorted(Comparator.comparing(RssFeedResponse::getPublishingDate).reversed())
+			.collect(Collectors.toList());
+		return sortedResponse;
 	}
 	
 	private void generateFeedBurnerDetails(List<RssFeedResponse> feedResponses) {
 		feedResponses.addAll(feedBurnerHandler.getFeedBurnerDetails());
+	}
+	
+	private void generateXkcdDetails(List<RssFeedResponse> feedResponses) {
+		feedResponses.addAll(XkcdHandler.getXkcdDetails());
 	}
 	
 }
